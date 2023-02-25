2d7d7f8897aaa3a5a8d818f849cc51353018220c
Gary Hu <garyhu728@gmail.com>
2023-02-24 20:38:44 -0500

    refactored examples

--- backend/catbot/examples.js
+++ backend/catbot/examples.js
@@ -2,67 +2,34 @@ const CAT_JOKE = 'Cat joke';
 const CAT_FACT = 'Cat fact';
 
 const catJokeExamples = [
-  {
-    label: CAT_JOKE,
-    text: 'I want a cat joke',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'Can I have a cat joke',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'I want a random cat joke',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'Cat joke please',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'Cat joke',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'Joke about a cat',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'Joke about cats please',
-  },
-  {
-    label: CAT_JOKE,
-    text: 'Supply me with a playful quip pertaining a feline creatue',
-  },
+  'I want a cat joke',
+  'Can I have a cat joke',
+  'I want a random cat joke',
+  'Cat joke please',
+  'Cat joke',
+  'Joke about a cat',
+  'Joke about cats please',
+  'Supply me with a playful quip pertaining a feline creatue',
 ];
 
 const catFactExamples = [
-  {
-    label: CAT_FACT,
-    text: 'I want a cat fact',
-  },
-  {
-    label: CAT_FACT,
-    text: 'Can I have a cat fact',
-  },
-  {
-    label: CAT_FACT,
-    text: 'I want a random cat fact',
-  },
-  {
-    label: CAT_FACT,
-    text: 'Cat fact please',
-  },
-  {
-    label: CAT_FACT,
-    text: 'Cat fact',
-  },
-  {
-    label: CAT_FACT,
-    text: 'Fact about a cat',
-  },
+  'I want a cat fact',
+  'Can I have a cat fact',
+  'I want a random cat fact',
+  'Cat fact please',
+  'Cat fact',
+  'Fact about a cat',
 ];
 
-const examples = [...catJokeExamples, ...catFactExamples];
+const exampleFactory = (label, examples) => {
+  return examples.map((example) => {
+    return { label, example };
+  });
+};
+
+const examples = [
+  ...exampleFactory(CAT_JOKE, catJokeExamples),
+  ...exampleFactory(CAT_FACT, catFactExamples),
+];
 
 export default examples;
