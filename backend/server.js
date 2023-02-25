353a61e018bd9f9f018d6223793247696e394bd1
unknown <matthewchun.18@gmail.com>
2023-02-24 21:24:59 -0500

    basic server code

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+// import mongoose from 'mongoose'
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
