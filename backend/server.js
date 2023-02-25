e43b98dcb3bb6234ae80e02b0827499a22d15883
Gary Hu <garyhu728@gmail.com>
2023-02-24 21:31:18 -0500

    change server port

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 // import mongoose from 'mongoose'
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
