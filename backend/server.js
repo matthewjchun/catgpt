89527037a3288eb5365efe9648c930ae6a89fd75
Gary Hu <garyhu728@gmail.com>
2023-02-25 19:02:21 -0500

    added a post route

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,11 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+//
+app.post('/chat', async (req, res) => {
+  // const query = req.body.query;
+  const query = 'tell me a cat joke';
+  const response = catbot.request([query]);
+  console.log(res);
+});
