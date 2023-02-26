898f319af6019521804bc60298f632f6e3befbbd
unknown <matthewchun.18@gmail.com>
2023-02-26 00:48:17 -0500

    debugging messaging flow

--- frontend/src/pages/Chat/components/Messaging/Messaging.js
+++ frontend/src/pages/Chat/components/Messaging/Messaging.js
@@ -4,10 +4,44 @@ import Input from '@mui/material/Input';
 import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import Avatar from '@mui/material/Avatar';
+import catAvi from '../../../../static/kitten1.png'
 import { MessagesContext } from '../../../../contexts/Messages'
 import axios from 'axios'
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import { pink } from '@mui/material/colors';
 
 let nextId = 0
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}));
 
 function Messaging() {
     const [messages, setMessages] = useContext(MessagesContext)
@@ -20,11 +54,24 @@ function Messaging() {
     const handleSendMessage = (e) => {
         e.preventDefault()
         setMessages([...messages, { key: nextId++, message: userMessage }])
+        console.log(messages)
         setUserMessage('')
+        handleCatResponse()
     }
 
-    const handleCatResponse = (e) => {
-        
+    const handleCatResponse = async () => {
+        const req = {
+            query: userMessage
+        }
+
+        await axios.post("http://localhost:4000/chat", req)
+            .then((response) => {
+                console.log('Chatbot responded successfully: ', response.data);
+                setMessages([...messages, { key: nextId++, message: response.data }])
+            })
+            .catch((error) => {
+                console.log('Error:', error);
+            })
     }
 
     return (
@@ -32,14 +79,37 @@ function Messaging() {
             <div>
                 <Box sx={{
                     width: '100%',
-                    height: '70vh',
+                    minHeight: '70vh',
                     marginBottom: '5vh',
                     backgroundColor: '#EFF6E0',
                 }}>
                     {messages.map(item =>
-                    (
-                        <p key={item.key}>{item.message}</p>
-                    ))}
+                        item.key / 2 === 0 ?
+                            (
+                                <div className="user-message">
+                                    <StyledBadge
+                                        overlap="circular"
+                                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                                        variant="dot"
+                                    >
+                                        <Avatar sx={{ bgcolor: pink[500] }} alt="CatGPT" src={catAvi} variant='rounded' />
+                                    </StyledBadge>
+                                    <p style={{ marginLeft: "2vh" }} key={item.key}>{item.message}</p>
+                                </div>
+                            )
+                            :
+                            (
+                                <div style={{ display: "flex", marginLeft: "3vh" }}>
+                                    <StyledBadge
+                                        overlap="circular"
+                                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                                        variant="dot"
+                                    >
+                                        <Avatar sx={{ bgcolor: pink[500] }} alt="CatGPT" src={catAvi} variant='rounded' />
+                                    </StyledBadge>
+                                    <p style={{ marginLeft: "2vh" }} key={item.key}>{item.message}</p>
+                                </div>
+                            ))}
                 </Box>
             </div>
             <form className='messaging-input' onSubmit={handleSendMessage} >
